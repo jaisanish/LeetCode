fcaea39702a4a4311e3cd754485c84fa97cae96a
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-07-25 01:02:30 +0530

    Time: 0 ms (100%), Space: 125.3 MB (54.9%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -2,8 +2,6 @@ class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n=nums.length;
         if(n<3)return n;
-        int bits=0;
-        while((1<<bits)<=n)bits++;
-        return 1<<bits;
+        return 1 << (32 - Integer.numberOfLeadingZeros(n));
     }
 }
