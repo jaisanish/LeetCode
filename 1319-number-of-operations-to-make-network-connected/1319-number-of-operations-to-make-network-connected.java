c96b416aa7cd523c7777b3ecb5e1211202d0dd8c
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-07-28 22:33:42 +0530

    Time: 5 ms (76.84%), Space: 66.1 MB (61.07%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,45 +1,58 @@
 class Solution {
+
     static class DisjointSet {
-        List<Integer> parent = new ArrayList<>();
-        List<Integer> size = new ArrayList<>();
-        public DisjointSet(int n) {
-            for (int i = 0; i <= n; i++) {
-                parent.add(i);
-                size.add(1);
+        int[] parent;
+        int[] size;
+        DisjointSet(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
             }
         }
 
-        public int findUPar(int node) {
-            if (node == parent.get(node)) {
+        int find(int node) {
+            if (parent[node] == node)
                 return node;
-            }
-            int ulp = findUPar(parent.get(node));
-            parent.set(node, ulp);
-            return parent.get(node);
+
+            return parent[node] = find(parent[node]); // Path Compression
         }
-        public void unionBySize(int u, int v) {
-            int ulp_u = findUPar(u);
-            int ulp_v = findUPar(v);
-            if (ulp_u == ulp_v) return;
-            if (size.get(ulp_u) < size.get(ulp_v)) {
-                parent.set(ulp_u, ulp_v);
-                size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+
+        void union(int u, int v) {
+            int pu = find(u);
+            int pv = find(v);
+
+            if (pu == pv)
+                return;
+
+            if (size[pu] < size[pv]) {
+                parent[pu] = pv;
+                size[pv] += size[pu];
             } else {
-                parent.set(ulp_v, ulp_u);
-                size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+                parent[pv] = pu;
+                size[pu] += size[pv];
             }
         }
     }
+
     public int makeConnected(int n, int[][] connections) {
-        if(n-1>connections.length)return -1;
-        DisjointSet ds=new DisjointSet(n);
-        for(int i=0;i<connections.length;i++){
-            ds.unionBySize(connections[i][0],connections[i][1]);
+
+        // Not enough cables to connect n computers
+        if (connections.length < n - 1)
+            return -1;
+
+        DisjointSet ds = new DisjointSet(n);
+
+        for (int[] edge : connections) {
+            ds.union(edge[0], edge[1]);
         }
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(ds.findUPar(i)==i)cnt++;
+
+        int components = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.find(i) == i) components++;
         }
-        return cnt==1?0:cnt-1;
+
+        return components - 1;
     }
 }
