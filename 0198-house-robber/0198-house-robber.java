744f3626b92f8110611d2de5002ffd2e3377a3d6
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-07-31 22:36:52 +0530

    Time: 0 ms (100%), Space: 42.9 MB (38.01%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
-        int next = 0;
-        int nextNext = 0;
+        if (nums.length == 0) return 0;
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            int curr = Math.max(nums[i] + nextNext, next);
-            nextNext = next;
-            next = curr;
+        // Initialize two tracking sums
+        int prev2 = 0;
+        int prev = nums[0];
+
+        // Traverse through the array
+        for (int i = 1; i < nums.length; i++) {
+            // Include current by adding it to two steps back
+            int include = nums[i] + prev2;
+
+            // Exclude current by taking previous best
+            int exclude = prev;
+
+            // Choose max of include and exclude
+            int curr = Math.max(include, exclude);
+
+            // Update tracking variables
+            prev2 = prev;
+            prev = curr;
         }
-        return next;
+
+        // Final result is stored in prev
+        return prev;
     }
 }
