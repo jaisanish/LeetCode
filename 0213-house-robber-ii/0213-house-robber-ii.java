6c578fd37593eead0e707e01f5e8d1473e8bae27
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-07-31 22:44:31 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.92%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,7 +2,8 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
 
-        if (n == 1) return nums[0];
+        if (n == 1)
+            return nums[0];
 
         return Math.max(
             robRange(nums, 0, n - 2),
@@ -10,16 +11,20 @@ class Solution {
         );
     }
 
-    private int robRange(int[] nums, int start, int end) {
-        int next = 0;
-        int nextNext = 0;
+    private int robRange(int[] nums, int s, int e) {
+        int prev2 = 0;
+        int prev = nums[s];
 
-        for (int i = end; i >= start; i--) {
-            int curr = Math.max(nums[i] + nextNext, next);
-            nextNext = next;
-            next = curr;
+        for (int i = s + 1; i <= e; i++) {
+            int include = nums[i] + prev2;
+            int exclude = prev;
+
+            int curr = Math.max(include, exclude);
+
+            prev2 = prev;
+            prev = curr;
         }
 
-        return next;
+        return prev;
     }
 }
