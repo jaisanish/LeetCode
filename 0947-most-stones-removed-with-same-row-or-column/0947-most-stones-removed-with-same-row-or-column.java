9e0706089d79e2fbf2de69d7cd6ffb2693a819c9
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-08-04 22:31:23 +0530

    Time: 11 ms (87.12%), Space: 47 MB (27.72%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,25 +1,28 @@
+import java.util.*;
+
 class Solution {
+
     static class DisjointSet {
         int[] parent;
         int[] size;
-        DisjointSet(int n) {
-            parent = new int[n];
-            size = new int[n];
+        DisjointSet(int n){
+            parent=new int[n];
+            size=new int[n];
             for(int i=0;i<n;i++){
                 parent[i]=i;
                 size[i]=1;
             }
         }
 
-        int find(int node){
-            if(parent[node]==node) return node;
-            return parent[node]=find(parent[node]);
+        int find(int x){
+            if(parent[x]==x)return x;
+            return parent[x]=find(parent[x]);
         }
 
         void union(int u,int v){
             int pu=find(u);
             int pv=find(v);
-            if(pu==pv) return;
+            if(pu==pv)return;
             if(size[pu]>=size[pv]){
                 parent[pv]=pu;
                 size[pu]+=size[pv];
@@ -31,19 +34,28 @@ class Solution {
     }
 
     public int removeStones(int[][] stones) {
-        int n=stones.length;
-        DisjointSet ds=new DisjointSet(n);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(stones[i][0]==stones[j][0] ||stones[i][1]==stones[j][1]){
-                    ds.union(i,j);
-                }
-            }
+        int maxRow=0;
+        int maxCol=0;
+
+        for(int[] stone:stones){
+            maxRow=Math.max(maxRow,stone[0]);
+            maxCol=Math.max(maxCol,stone[1]);
+        }
+
+        DisjointSet ds=new DisjointSet(maxRow+maxCol+2);
+        HashSet<Integer> used=new HashSet<>();
+        for(int[] stone:stones){
+            int row=stone[0];
+            int col=stone[1]+maxRow+1;
+            ds.union(row,col);
+            used.add(row);
+            used.add(col);
         }
         int components=0;
-        for(int i=0;i<n;i++){
-            if(ds.find(i)==i) components++;
+        for(int node:used){
+            if(ds.find(node)==node)
+                components++;
         }
-        return n-components;
+        return stones.length-components;
     }
 }
