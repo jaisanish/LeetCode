038aee18e3e538339e36a388aaf22e1de876ec57
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-08-05 21:47:27 +0530

    Time: 11 ms (63.3%), Space: 46.4 MB (46.09%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,33 +1,30 @@
 class Solution {
     public int swimInWater(int[][] grid) {
+        int directions[][]={{0,1},{1,0},{0,-1},{-1,0}};
+        int n=grid.length;
+
         PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->Integer.compare(a[2],b[2]));
         pq.offer(new int[]{0,0,grid[0][0]});
-        int n=grid.length;
         boolean vis[][]=new boolean[n][n];
+        vis[0][0] = true;
 
-        int directions[][]={{0,1},{1,0},{0,-1},{-1,0}};
-        while (!pq.isEmpty()) {
+
+        while(!pq.isEmpty()){
             int[] cur = pq.poll();
 
-            int row = cur[0];
-            int col = cur[1];
-            int time = cur[2];
+            if(cur[0]==n-1 && cur[1]==n-1)
+                return cur[2];
 
-            if (vis[row][col]) continue;
-            vis[row][col] = true;
+            for(int[] d:directions){
+                int nr=cur[0]+d[0];
+                int nc=cur[1]+d[1];
 
-            if (row == n - 1 && col == n - 1)
-                return time;
-
-            for (int[] d : directions) {
-                int nr = row + d[0];
-                int nc = col + d[1];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !vis[nr][nc]) {
+                if(nr>=0 && nr<n && nc>=0 && nc<n && !vis[nr][nc]){
+                    vis[nr][nc]=true;
                     pq.offer(new int[]{
                         nr,
                         nc,
-                        Math.max(time, grid[nr][nc])
+                        Math.max(cur[2],grid[nr][nc])
                     });
                 }
             }
