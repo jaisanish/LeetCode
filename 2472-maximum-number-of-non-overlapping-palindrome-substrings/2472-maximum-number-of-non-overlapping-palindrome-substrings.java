42a9dc67f1b1bd55aa4f25e60e4ede61edece424
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-09-16 00:30:50 +0530

    Time: 402 ms (5.03%), Space: 210.5 MB (5.03%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,24 +1,42 @@
 class Solution {
+    boolean isPalindrome[][];
+
     public int maxPalindromes(String s, int k) {
-        if(k==1)return s.length();
-        int dp[][]=new int[s.length()][s.length()];
+        int n=s.length();
+        if(k==1)return n;
+
+        int dp[][]=new int[n][n];
         for(int[] row:dp)Arrays.fill(row,-1);
-        return solve(s,k,0,k-1,dp);
+
+        isPalindrome=new boolean[n+1][n+1];
+        for(int L=1;L<=n;L++){
+            for(int i=0;i+L<=n;i++){
+                int j=i+L-1;
+                if(i==j){ //length 1
+                    isPalindrome[i][j]=true;
+                }
+                else if(i+1==j){ //length 2
+                    isPalindrome[i][j]=s.charAt(i)==s.charAt(j);
+                }
+                else{
+                    isPalindrome[i][j]=(s.charAt(i)==s.charAt(j)) && isPalindrome[i+1][j-1];
+                }
+            }
+        }
+
+
+        return solve(s,n,k,0,k-1,dp);
     }
-    public int solve(String s,int k, int i,int j,int[][]dp){
-        if(i>=s.length() || j>=s.length())return 0;
+
+    public int solve(String s,int n,int k, int i,int j,int[][]dp){
+        if(i>=n || j>=n)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
+
         int take=0;
-        if(isPalindrome(s,i,j))take=1+solve(s,k,j+1,j+k,dp);
-        int grow=solve(s,k,i,j+1,dp);
-        int slide=solve(s,k,i+1,j+1,dp);
+        if(isPalindrome[i][j])take=1+solve(s,n,k,j+1,j+k,dp);
+        int grow=solve(s,n,k,i,j+1,dp);
+        int slide=solve(s,n,k,i+1,j+1,dp);
 
         return dp[i][j]=Math.max(take,Math.max(grow,slide));
-    }
-    public boolean isPalindrome(String s,int i,int j){
-        while(i<j){
-            if(s.charAt(i++)!=s.charAt(j--))return false;
-        }
-        return true;
     }
 }
