5a5b7634e87b050b91ac6cfa2c309306ded52f75
ANISH JAISWAL <anish.12.jaiswal@gmail.com>
2026-09-20 13:15:51 +0530

    Time: 1 ms (100%), Space: 44.1 MB (34.29%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            int value=(26-(ch-'a'))*(i+1);
+            ans+=value;
+        }
+        return ans;
+    }
+}
